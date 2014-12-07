499bae949f9aad57808a80086e15913b77824d71
Alex Panin <alexander.panin@eagles.ewu.edu>
2014-12-07 14:55:31 -0800

    TheGame import
    Getting the project folder out

---
+++ TheGame/Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxing : MonoBehaviour
+{
+
+    public Transform[] backgrounds;             // Array of all the back- and foregrounds to be parallaxed
+    private float[] parallaxScales;             // The proportion of the camera's movement to move the backgrounds by
+    public float smoothing = 1f;                // How smooth the parallax is going to be. Has to be above 0
+
+    private Transform cam;                      // Reference to the main camera's transform
+    private Vector3 previousCamPos;             // The position of the camera in the previous frame
+
+    // Called before Start(). Great for references
+    void Awake()
+    {
+        // set up the camera reference
+        cam = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // The previous frame had the current frame's camera position
+        previousCamPos = cam.position;
+
+        // Assigning corresponding parallaxScales
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales [i] = backgrounds [i].position.z * -1;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // For each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            // The parallax is the opposite of the camera movement
+            // because the previous frame multiplied by the scale
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales [i];
+
+            // Set a target x position which is current position plus the parallax
+            float backgrounTargetPosX = backgrounds [i].position.x + parallax;
+
+            // Create a target position which is the background's current position with it's target x position
+            Vector3 backgrounTargetPos = new Vector3(backgrounTargetPosX, 
+                                                      backgrounds [i].position.y, 
+                                                      backgrounds [i].position.z);
+
+            // Fade between current position and the target position using lerp
+            backgrounds [i].position = Vector3.Lerp(backgrounds [i].position,
+                                                   backgrounTargetPos,
+                                                   smoothing * Time.deltaTime);
+        }
+
+        // Set the previousCamPos to the camera's position at the end of the frame
+        previousCamPos = cam.position;
+    }
+}
---
+++ TheGame/Assets/Scripts/Tiling.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour
+{
+    public int offsetX = 2;                 // The offset so that we don't get any weird errors
+    public bool hasARightBuddy = false;     // Used if we need to instantiate stuff
+    public bool hasALeftBuddy = false;      // Used if we need to instantiate stuff
+    public bool reverseScale = false;       // Used if object is not tilable
+    private float spriteWidth = 0f;         // The width of our element
+    private Camera cam;
+    private Transform myTransform;
+
+    void Awake()
+    {
+        cam = Camera.main;
+        myTransform = transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = sRenderer.sprite.bounds.size.x;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Does it still need Buddies? If not, do nothing
+        if (hasALeftBuddy == false || hasARightBuddy == false)
+        {
+            // Calculate the cameras extend (half the width) of what the camera can see in world coordinates
+            float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+
+            // Calculate the x position where the camera can see the edge of the sprite (element)
+            float edgeVisiblePositionRight = (myTransform.position.x + spriteWidth / 2) - camHorizontalExtend;
+            float edgeVisiblePositionLeft = (myTransform.position.x - spriteWidth / 2) + camHorizontalExtend;
+
+            // Checking if we can see the edge of the element
+            if (cam.transform.position.x >= edgeVisiblePositionRight - offsetX && hasARightBuddy == false)
+            {
+                makeNewBuddy(1);
+                hasARightBuddy = true;
+            } else if (cam.transform.position.x <= edgeVisiblePositionLeft + offsetX && hasALeftBuddy == false)
+            {
+                makeNewBuddy(-1);
+                hasALeftBuddy = true;
+            }
+        }
+    }
+
+    // a function that creates a buddy on the side required
+    void makeNewBuddy(int rightOrLeft)
+    {
+        // Calculating the new position for our new buddy
+        Vector3 newPosition = new Vector3(myTransform.position.x + spriteWidth * rightOrLeft, 
+                                          myTransform.position.y,
+                                          myTransform.position.z);
+
+        // Instantiating our new buddy and storing him in the variable
+        Transform newBuddy = Instantiate(myTransform, newPosition, myTransform.rotation) as Transform;
+
+        // If not tilable let's reverse the x size of our object
+        if (reverseScale == true)
+        {
+            newBuddy.localScale = new Vector3(newBuddy.localScale.x * -1,
+                                               newBuddy.localScale.y,
+                                               newBuddy.localScale.z);
+        }
+
+        newBuddy.parent = myTransform.parent;
+        if (rightOrLeft > 0)
+        {
+            newBuddy.GetComponent<Tiling>().hasALeftBuddy = true;
+        } else
+        {
+            newBuddy.GetComponent<Tiling>().hasARightBuddy = true;
+        }
+    }
+}
