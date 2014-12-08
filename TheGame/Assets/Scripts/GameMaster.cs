6819ddafed5e8a469285c267ffa9920656aa7fdf
Alex Panin <alexander.panin@eagles.ewu.edu>
2014-12-07 16:01:31 -0800

    12/7/2014
    Add particles and sound on spawn

--- TheGame/Assets/Scripts/GameMaster.cs
+++ TheGame/Assets/Scripts/GameMaster.cs
@@ -13,15 +13,17 @@ public class GameMaster : MonoBehaviour
 
     public Transform playerPrefab;
     public Transform spawnPoint;
-    public int spawnDelay = 2;
+    public int spawnDelay = 4;
+    public Transform spawnPrefab;
 
     public IEnumerator RespawnPlayer () 
     {
-        Debug.Log("TODO: Add waiting for spawn sound");
+        audio.Play();
         yield return new WaitForSeconds(spawnDelay);
 
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
-        Debug.Log("TODO: Add Spawn Particles");
+        GameObject clone  = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+        Destroy (clone, 3f);
     }
 
     public static void KillPlayer (Player player) 
